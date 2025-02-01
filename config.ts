07fe233996ea5f9e63969475287ae63f7d546720
ZERO DEV <romewatchtv@gmail.com>
2025-02-02 00:56:26 +0500

    test

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-import publicKey from "./public.pem";
+import token from './public.pem';
 
 export const window = {
     width: 900,
@@ -13,7 +13,7 @@ export const window = {
 export const api = {
     ws: 'ws://84.252.159.218:24454/ws',
     web: 'http://84.252.159.218:24454',
-    publicKey,
+    extraToken: token,
 };
 
 export const appPath = '.aurora-launcher';
